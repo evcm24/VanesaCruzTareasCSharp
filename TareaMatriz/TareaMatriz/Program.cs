f3074185ca33e8703a04443135d071ce2fed7e9d
evcm24 <33740136+evcm24@users.noreply.github.com>
2017-12-04 22:37:24 -0600

    Add files via upload

---
+++ TareaMatriz/TareaMatriz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TareaMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] matr = {{5,7,6},{5,4,1},{7,9,0}};
+            int diagonal=0;
+
+            for (int i = 0; i < 3; i++) 
+            {
+                diagonal = diagonal + matr[i, i];
+                if (i != 1)
+                    diagonal = diagonal + matr[i, 2];
+            }
+            Console.WriteLine("La diagonal es: " + diagonal);
+            Console.ReadLine();
+
+        }
+    }
+}
